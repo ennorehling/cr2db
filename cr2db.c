c0c75436a6bb7bfc24c65c75de247675607fe94b
Enno Rehling <enno.rehling@gmail.com>
2019-02-18 13:27:41 +0100

    bad argument parsing

--- cr2db.c
+++ cr2db.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
     crdb_t data;
 
     crdb_init(&data);
-    if (0 == crdb_import(&data, "test.cr")) {
+    if (0 == crdb_import(&data, argv[1])) {
         char *dump = cJSON_Print(data.json);
         puts(dump);
         cJSON_free(dump);
