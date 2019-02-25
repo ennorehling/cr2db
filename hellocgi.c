c5e570d755abebd963d530e61a1f10aced190a2e
Enno Rehling <enno.rehling@gmail.com>
2019-02-25 20:54:27 +0100

    cgiapp.h is the one I wanted

--- hellocgi.c
+++ hellocgi.c
@@ -1,12 +1,12 @@
-#include <fastcgi.h>
+#include <fcgiapp.h>
 
 int main(int argc, char **argv) {
-    FCGX_Request request;
+    FCGX_Request req;
     FCGX_Init();
-    FCGX_InitRequest(&request, 0, 0);
-    while(FCGX_Accept_r(&request) == 1) {
-        FCGX_PutS("Content-Type: text/plain\r\n\r\n", Req.pOut);
-        FCGX_PutS("Hello World\n", Req.pOut);
+    FCGX_InitRequest(&req, 0, 0);
+    while(FCGX_Accept_r(&req) == 1) {
+        FCGX_PutS("Content-Type: text/plain\r\n\r\n", req.out);
+        FCGX_PutS("Hello World\n", req.out);
     }
 
     return 0;
