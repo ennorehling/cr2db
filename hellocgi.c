36522bdca2e0d1121d111426b8d9f883bf59d3be
Enno Rehling <enno.rehling@gmail.com>
2019-03-14 18:24:46 +0100

    hello can echo environment

--- hellocgi.c
+++ hellocgi.c
@@ -7,8 +7,12 @@ int main(void) {
     err = FCGX_Init();
     err = FCGX_InitRequest(&req, 0, 0);
     while((err = FCGX_Accept_r(&req)) >= 0) {
+        int e;
         FCGX_PutS("Content-Type: text/plain\r\n\r\n", req.out);
         FCGX_FPrintF(req.out, "Hello World, request #%d\n", ++i);
+        for (e = 0; req.envp[e]; ++e) {
+            FCGX_FPrintF(req.out, "%s\n", req.envp[e]);
+        }
     }
 
     fprintf(stderr, "hellocgi done with err=%d\n", err);
