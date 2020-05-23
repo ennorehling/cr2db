477ad6606d055ff4edd3556296122283ac3aac49
Enno Rehling <enno.rehling@gmail.com>
2020-05-23 09:52:34 +0200

    remove direct.h, update crpat

--- config.h
+++ config.h
@@ -6,5 +6,5 @@ int config_init(struct sqlite3 *db);
 int config_done(void);
 
 const char *config_get(const char *key, const char *def);
-void config_set(const char *key, const char *value);
-void config_delete(const char *key);
+int config_set(const char *key, const char *value);
+int config_delete(const char *key);
